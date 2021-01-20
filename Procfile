d4098321d9a453252ec476a544c3a49215261d50
ClementineSzalavecz <clementine.szalavecz@gmail.com>
2021-01-20 12:22:50 +0100

    add profile and remove requirements

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn inf_file:app
--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import tensorflow as tf
 import json
 import cv2
 
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict/',methods=['POST'])
+
+@app.route('/predict/', methods=['POST'])
 def predict_one(img, model, print_all=False, plot_img=False):
     resized = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
     preprocessed = preprocess_input(resized)
@@ -23,7 +27,7 @@ def predict_one(img, model, print_all=False, plot_img=False):
     class_names = {
         0: 'akiec',  # actinic keratoses and intraepithelial carcinoma/Bowen disease (akiec)
         1: 'bcc',  # basal cell carcinoma (bcc) *
-        2: 'bkl', # benign lesions of the keratosis type
+        2: 'bkl',  # benign lesions of the keratosis type
         3: 'df',  # dermatofibroma (df)
         4: 'mel',  # melanoma (mel) *
         5: 'nv',  # melanocytic nevi (nv)
@@ -56,17 +60,19 @@ def predict_one(img, model, print_all=False, plot_img=False):
 
     return (pred_name_class, pred_class, pred_R)
 
+
 # returns a compiled model
 def top_3_accuracy(y_true, y_pred):
     return top_k_categorical_accuracy(y_true, y_pred, k=3)
 
+
 def top_2_accuracy(y_true, y_pred):
     return top_k_categorical_accuracy(y_true, y_pred, k=2)
+
 
 if __name__ == '__main__':
     print('Main')
     load_path = 'skin_model.h5'
     model = load_model(load_path, custom_objects={"top_2_accuracy": top_2_accuracy, "top_3_accuracy": top_3_accuracy})
     print('Model is loaded', type(model))
-
-
+    app.run(host='0.0.0.0')
